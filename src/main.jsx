5b38afbb9e0c34a77cca61b0dd8f58faada0c915
k1k3cb <enriquecbar@gmail.com>
2023-09-27 01:53:42 +0200

    data fetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
 
 const container = document.getElementById('root');
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+	<CssBaseline>
+		<SnackbarProvider>
+			<App />
+		</SnackbarProvider>
+	</CssBaseline>
+);
